ced994703845e754b9c0f2204ec6ec98d249ad06
Simas <simonas.deimantavicius@mediapark.com>
2019-08-28 14:05:51 +0300

    add express

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const http = require('http');
+const express = require('express');
+
+const app = express();
 
 const routes = require('./routes');
 
 console.log(routes.someText);
 
-const server = http.createServer(routes.handler);
+const server = http.createServer(app);
 
 server.listen(3000);
